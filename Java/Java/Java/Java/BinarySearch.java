3d54c1070a41427b0b1d2dbb4f0fc8f9d9eb1b2f
kaviyakaviya3 <kaviyakaviya3315@gmail.com>
2026-09-01 20:26:24 +0530

    Create BinarySearch.java

---
+++ Java/Java/Java/Java/BinarySearch.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements in sorted order:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter element to search: ");
+        int search = sc.nextInt();
+
+        int low = 0;
+        int high = n - 1;
+        boolean found = false;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == search) {
+                System.out.println("Element found at index: " + mid);
+                found = true;
+                break;
+            } 
+            else if (arr[mid] < search) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
